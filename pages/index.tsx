c687a1a153a9cc7847b9474e8efb3d144160749d
codewriter73 <mason.fd.c@gmail.com>
2022-07-31 19:25:23 -0400

    feat: added Yahtzee page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import ColorSchemeToggle from '@/components/ColorSchemeToggle';
+import YahtzeeGame from '@/components/YahtzeeGame';
 import { Card, Stack, Center } from '@mantine/core';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => (
-  <Center style={{ height: '100vh' }}>
+  <Center style={{ minHeight: '100%' }}>
     <Card radius="md" shadow="md" withBorder>
       <Stack align="center">
         <ColorSchemeToggle />
+        <YahtzeeGame />
       </Stack>
     </Card>
   </Center>
