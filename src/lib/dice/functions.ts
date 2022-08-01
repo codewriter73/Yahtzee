e5abbb37bf0a8b9d0ffba137235cd938dc30c204
codewriter73 <mason.fd.c@gmail.com>
2022-07-31 20:25:57 -0400

    refactor: rollDice from Math to Crypto

--- src/lib/dice/functions.ts
+++ src/lib/dice/functions.ts
@@ -1,11 +1,39 @@
 import { SIDED_DICE } from './constants';
 import { Dice, DiceObject } from './types';
 
+const U8BIT_MAX = 255;
+
 export function rollDice(num: number): Dice {
-  const result: number[] = [];
-  for (let i = 0; i < num; i += 1) {
-    result.push(Math.floor(Math.random() * SIDED_DICE) + 1);
+  let resultArr = window.crypto.getRandomValues(new Uint8Array(num));
+
+  // The range for each int is 0-255 -> 42.5 intervals of 6
+  // This means we have slightly higher chance of getting the numbers 1 to 3
+  // we keep generating random arrays until all numbers have an equal chance
+  // It will only recalculate 1.6% of the time
+  // We can expand this to work for any sided dice;
+
+  const mod = (U8BIT_MAX % SIDED_DICE) + 1;
+  // the +1 is needed is because 0 is the start of the range
+  const maxArray = Array.from({ length: mod }, (_, i) => i);
+  const isUneven = (arr: Uint8Array) => {
+    let even = false;
+    maxArray.forEach((v) => {
+      if (arr.includes(U8BIT_MAX - v)) {
+        even = true;
+      }
+    });
+    return even;
+  };
+
+  while (isUneven(resultArr)) {
+    resultArr = window.crypto.getRandomValues(new Uint8Array(num));
   }
+  // convert to dice numbers
+  const result: Dice = Array(num).fill(0);
+  for (let i = 0; i < num; i += 1) {
+    result[i] = (resultArr[i] % SIDED_DICE) + 1;
+  }
+
   return result;
 }
 
